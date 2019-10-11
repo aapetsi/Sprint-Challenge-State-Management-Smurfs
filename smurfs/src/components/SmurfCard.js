0eef570f5c03cd2e91afd3751e047f0bed50bd37
aapetsi <apetsiampiah@gmail.com>
2019-10-11 10:38:22 +0000

    Add styling for smurf cards

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { deleteSmurf } from "../actions/smurfs";
+import React from 'react'
+import { connect } from 'react-redux'
+import { deleteSmurf } from '../actions/smurfs'
 
 const SmurfCard = ({ id, name, age, height, dispatch }) => {
-  const smurfToDelete = { id, name, age, height };
+  const smurfToDelete = { id, name, age, height }
   const handleClick = () => {
-    dispatch(deleteSmurf(smurfToDelete));
-  };
+    dispatch(deleteSmurf(smurfToDelete))
+  }
   return (
-    <div className="card">
+    <div className='card'>
       <p>Name: {name}</p>
       <p>Age: {age}</p>
       <p>Height: {height}</p>
       <button onClick={handleClick}>Delete</button>
     </div>
-  );
-};
+  )
+}
 
 export default connect(
   null,
   null
-)(SmurfCard);
+)(SmurfCard)
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,14 +1,13 @@
-import React from "react";
-import SmurfCard from "./SmurfCard";
-import { connect } from "react-redux";
+import React from 'react'
+import SmurfCard from './SmurfCard'
+import { connect } from 'react-redux'
 
 const SmurfsList = props => {
-  console.log(props.smurfs);
   return (
     <div>
       <h1>all smurfs</h1>
       {props.error && <p>{props.error}</p>}
-      <div className="card-container">
+      <div className='card-container'>
         {props.smurfs.map(smurf => (
           <SmurfCard
             key={smurf.id}
@@ -20,15 +19,15 @@ const SmurfsList = props => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
 const mapStateToProps = state => ({
   smurfs: state.smurfs,
   error: state.error
-});
+})
 
 export default connect(
   mapStateToProps,
   null
-)(SmurfsList);
+)(SmurfsList)
