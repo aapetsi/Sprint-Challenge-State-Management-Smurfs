f1421ca30c390a80142e15d30bc25cd1bdbee2a5
aapetsi <apetsiampiah@gmail.com>
2019-10-11 10:27:39 +0000

    Add simple styling

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import SmurfCard from './SmurfCard'
-import { connect } from 'react-redux'
+import React from "react";
+import SmurfCard from "./SmurfCard";
+import { connect } from "react-redux";
 
 const SmurfsList = props => {
-  console.log(props.smurfs)
+  console.log(props.smurfs);
   return (
     <div>
       <h1>all smurfs</h1>
       {props.error && <p>{props.error}</p>}
-      {props.smurfs.map(smurf => (
-        <SmurfCard
-          key={smurf.id}
-          name={smurf.name}
-          id={smurf.id}
-          age={smurf.age}
-          height={smurf.height}
-        />
-      ))}
+      <div className="card-container">
+        {props.smurfs.map(smurf => (
+          <SmurfCard
+            key={smurf.id}
+            name={smurf.name}
+            id={smurf.id}
+            age={smurf.age}
+            height={smurf.height}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   smurfs: state.smurfs,
   error: state.error
-})
+});
 
 export default connect(
   mapStateToProps,
   null
-)(SmurfsList)
+)(SmurfsList);
